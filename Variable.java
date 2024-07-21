f48aca9610105afb04ae0cb7facd8f4c46b64ff2
shivkant-epam <shivkantkr9934@gmail.com>
2024-07-21 19:01:09 +0530

    Java basics part :- Variable

---
+++ Variable.java
@@ -0,0 +1,19 @@
+class Variable {
+    static int employee_id = 10; // static varibale :- called with class.variable_name
+    int pincode = 803202; // instance variable
+
+    void employee_details(String name) {
+        String address = "Patna";// local variable 
+        System.out.println("My name is " + name + " and i am from " + address);
+
+    }
+
+    public static void main(String args[]) {
+        Variable obj = new Variable();
+        obj.employee_details("Shiv kant");
+
+        System.out.println("Employee_id:" + Variable.employee_id); // calling static variable
+        System.out.println("Pincode:" + obj.pincode); // instance variable
+
+    }
+}
